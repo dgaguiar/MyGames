da429c91da6e7d3438940fcf8e2088d8dc6a7112
Daianne <daiannegaguiar@icloud.com>
2021-04-01 15:17:26 -0300

    add layout

---
+++ MyGames/MyGames/AddEditViewController.swift
@@ -0,0 +1,41 @@
+//
+//  AddEditViewController.swift
+//  MyGames
+//
+//  Created by User on 01/04/21.
+//  Copyright © 2021 User. All rights reserved.
+//
+
+import UIKit
+
+class AddEditViewController: UIViewController {
+    
+    @IBOutlet weak var tfTitle: UITextField!
+    @IBOutlet weak var tfConsole: UITextField!
+    @IBOutlet weak var dpReleaseDate: UIDatePicker!
+    @IBOutlet weak var btAddEdit: UIButton!
+    @IBOutlet weak var btCover: UIButton!
+    @IBOutlet weak var ivCover: UIImageView!
+    
+    @IBAction func addEditCover(_ sender: Any) {
+    }
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ MyGames/MyGames/GameViewController.swift
@@ -0,0 +1,35 @@
+//
+//  GameViewController.swift
+//  MyGames
+//
+//  Created by User on 01/04/21.
+//  Copyright © 2021 User. All rights reserved.
+//
+
+import UIKit
+
+class GameViewController: UIViewController {
+    
+    @IBOutlet weak var lbTitle: UILabel!
+    @IBOutlet var lbConsole: UIView!
+    @IBOutlet weak var lbReleaseDate: UILabel!
+    @IBOutlet weak var ivCover: UIImageView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
